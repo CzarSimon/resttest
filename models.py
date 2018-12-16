36757576cb5cb06a44e11d91fabc542a9a73aa53
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-12-16 22:46:41 +0100

    Added from dict constructors for models

--- models.py
+++ models.py
@@ -8,13 +8,26 @@ class Env:
     base_url: str
     data: Dict[str, str]
 
+    @classmethod
+    def fromdict(cls, raw: Dict[str, Any]) -> "cls":
+        return cls(base_url=raw["baseUrl"], data=raw["data"])
+
 
 @dataclass(frozen=True)
 class Request:
     method: str
     path: str
     body: Optional[Dict[str, Any]]
-    useToken: bool
+    use_token: bool
+
+    @classmethod
+    def fromdict(cls, raw: Dict[str, Any]) -> "cls":
+        return cls(
+            method=raw["method"],
+            path=raw["raw"],
+            body=raw["body"] if "body" in raw else None,
+            use_token=raw["useToken"],
+        )
 
 
 @dataclass(frozen=True)
@@ -22,11 +35,19 @@ class Response:
     status: int
     body: Optional[Dict[str, Any]]
 
+    @classmethod
+    def fromdict(cls, raw: Dict[str, Any]) -> "cls":
+        return cls(method=raw["status"], body=raw["body"] if "body" in raw else None)
+
 
 @dataclass(frozen=True)
 class EnvUpdate:
     env_key: str
     response_key: str
+
+    @classmethod
+    def fromdict(cls, raw: Dict[str, Any]) -> "cls":
+        return cls(env_key=raw["env_key"], response_key=raw["response_key"])
 
 
 @dataclass(frozen=True)
@@ -34,4 +55,17 @@ class TestCase:
     name: str
     request: Request
     response: Response
-    set_env: Optional[List[EnvUpdate]]
+    set_env: List[EnvUpdate]
+
+    @classmethod
+    def fromdict(cls, raw: Dict[str, Any]) -> "cls":
+        updates: List[EnvUpdate] = []
+        if "setEnv" in raw:
+            updates = [EnvUpdate.fromdict(u) for u in raw["setEnv"]]
+
+        return cls(
+            name=raw["name"],
+            request=Request.fromdict(raw["request"]),
+            response=Response.fromdict(raw["response"]),
+            set_env=updates,
+        )
